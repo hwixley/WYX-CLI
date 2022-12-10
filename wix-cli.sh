171eb0a4a626534b874e4336878d8a4512a8b100
Harry Wixley <hwixley1@gmail.com>
2022-12-10 22:47:35 +0000

    wix-cli quick commit

--- wix-cli.sh
+++ wix-cli.sh
@@ -2,66 +2,87 @@
 GREEN=$(tput setaf 2)
 RED=$(tput setaf 1)
 RESET=$(tput setaf 7)
-echo ""
-echo -e "${GREEN} ██╗    ██╗██╗██╗  ██╗     ██████╗██╗     ██╗ "
-echo -e "${GREEN} ██║    ██║██║╚██╗██╔╝    ██╔════╝██║     ██║ "
-echo -e "${GREEN} ██║ █╗ ██║██║ ╚███╔╝     ██║     ██║     ██║ "
-echo -e "${GREEN} ██║███╗██║██║ ██╔██╗     ██║     ██║     ██║ "
-echo -e "${GREEN} ╚███╔███╔╝██║██╔╝ ██╗    ╚██████╗███████╗██║ "
-echo -e "${GREEN}  ╚══╝╚══╝ ╚═╝╚═╝  ╚═╝     ╚═════╝╚══════╝╚═╝ "
-echo ""
 
-my_path=~/Documents/random-coding-projects/bashing
-gs_path=~/Documents/GetSkooled
 num_args="$#"
+mypath=~/Documents/random-coding-projects/bashing/wix-cli.sh
+
+declare -A mydirs
+mydirs["docs"]=~/Documents
+mydirs["bashing"]=~/Documents/random-coding-projects/bashing
+mydirs["gs"]=~/Documents/GetSkooled
+mydirs["gs-website"]=${mydirs["gs"]}/website/GetSkooled-MVP-Website
+mydirs["down"]=~/Downloads
+mydirs["pix"]=~/Pictures
+mydirs["rcp"]=~/Documents/random-coding-projects
+
+
 if [ $num_args -lt 1 ]; then
-	echo "Where do you want to go?${RESET}"
-	read dir
-	if [ "$dir" = "docs" ]; then
-		cd ~/Documents
-	elif [ "$dir" = "bashing" ]; then
-		cd $my_path
-	elif [ "$dir" = "gs" ]; then
-		cd $gs_path
-	elif [ "$dir" = "gs-website" ]; then
-		cd $gs_path/website/GetSkooled-MVP-Website
-	elif [ "$dir" = "down" ]; then
-		cd ~/Downloads
+	echo "Welcome to the..."
+	echo ""
+	echo -e "${GREEN} ██╗    ██╗██╗██╗  ██╗     ██████╗██╗     ██╗ "
+	echo -e "${GREEN} ██║    ██║██║╚██╗██╔╝    ██╔════╝██║     ██║ "
+	echo -e "${GREEN} ██║ █╗ ██║██║ ╚███╔╝     ██║     ██║     ██║ "
+	echo -e "${GREEN} ██║███╗██║██║ ██╔██╗     ██║     ██║     ██║ "
+	echo -e "${GREEN} ╚███╔███╔╝██║██╔╝ ██╗    ╚██████╗███████╗██║ "
+	echo -e "${GREEN}  ╚══╝╚══╝ ╚═╝╚═╝  ╚═╝     ╚═════╝╚══════╝╚═╝ "
+	echo ""
+	echo "COMMANDS:${RESET}"
+	echo "- dir <cdir> : navigation"
+	echo "- back : return to last dir"
+	echo "- new <cdir> : new directory"
+	echo "- run <cdir> : setup and run environment"
+	echo "- delete <cdir> : delete dir"
+	echo ""
+	echo "${GREEN}GITHUB AUTOMATION:${RESET}"
+	echo "- push"
+	echo "- git-init"
+	echo "- new-git <cdir>"
+	echo "- repo"
+	echo ""
+	echo "${GREEN}CLI management:${RESET}"
+	echo "- edit"
+	echo "- save"
+	echo "- cat"
+	echo "- cdir"
+
+elif [ "$1" = "dir" ]; then
+	if [ $num_args -gt 1 ]; then
+		if [ -v mydirs[$2] ]; then
+			echo "${GREEN}Travelling to -> ${mydirs[$2]}"
+			cd ${mydirs[$2]}
+		else
+			echo "${GREEN}That path is not supported try: docs, bashing, gs, gs-website, down"
+		fi
 	else
-		echo "${GREEN}That path is not supported try: docs, bashing, gs, gs-website, down"
+		echo "${GREEN}Where do you want to go?${RESET}"
+		read dir
+		if [ -v mydirs[$dir] ]; then
+			echo "${GREEN}Travelling to -> ${mydirs[$dir]}"
+			cd ${mydirs[$dir]}
+		else
+			echo "${GREEN}That path is not supported try: docs, bashing, gs, gs-website, down"
+		fi
 	fi
 elif [ "$1" = "new" ]; then
 	if [ "$2" = "gs" ]; then
-		echo "Generating new GetSkooled dir ($gs_path/$3)..."
+		echo "${GREEN}Generating new GetSkooled dir ($gs_path/$3)..."
 		mkdir $gs_path/$3
 		cd $gs_path/$3
 	else
-		echo "This is only supported for gs currently"
+		echo "${GREEN}This is only supported for gs currently"
 	fi
-elif [ "$1" = "new-git" ]; then
-	if [ "$2" = "gs" ]; then
-		echo "Generating new GetSkooled dir ($gs_path/$3)..."
-		mkdir $gs_path/$3
-		cd $gs_path/$3
-		echo "# $3" >> README.md
-		git init
-		git add .
-		git commit -m "first commit"
-		git remote add origin git@github.com:getskooled/$3.git
-		xdg-open https://github.com/organizations/getskooled/repositories/new
-	else
-		echo "This is only supported for gs currently"
-	fi
+	
 elif [ "$1" = "run" ]; then
 	if [ "$2" = "gs" ]; then
-		echo "Running GetSkooled localhost server on develop"
+		echo "${GREEN}Running GetSkooled localhost server on develop"
 		cd $gs_path/website/GetSkooled-MVP-Website
 		git checkout develop
 		git pull origin develop
 		php -S localhost:8081
 	else
-		echo "This is only supported for gs currently"
+		echo "${GREEN}This is only supported for gs currently"
 	fi
+	
 elif [ "$1" = "delete" ]; then
 	if [ "$2" = "gs" ]; then
 		echo "${RED}Are you sure you want to delete $gs_path/$3? [ Yy / Nn]${RESET}"
@@ -79,16 +100,46 @@ elif [ "$1" = "delete" ]; then
 	else
 		echo "This is only supported for gs currently"
 	fi
+	
+	
 elif [ "$1" = "edit" ]; then
-	echo "Edit wix-cli script..."
+	echo "${GREEN}Edit wix-cli script..."
 	gedit $my_path/wix-cli.sh
 	echo "Saving changes"
 	source ~/.bashrc
+	
 elif [ "$1" = "save" ]; then
-	echo "Sourcing bash :)"
+	echo "${GREEN}Sourcing bash :)"
 	source ~/.bashrc
+	
 elif [ "$1" = "cat" ]; then
 	cat $my_path/wix-cli.sh
+
+elif [ "$1" = "cdir" ]; then
+	echo "${GREEN}Enter an alias for your new directory:${RESET}"
+	read alias
+	echo "${GREEN}Enter the directory:${RESET}"
+	read i_dir
+	echo "${GREEN}Adding $alias=$i_dir to custom dirs"
+	sed -i "15imydirs["$alias"]=$i_dir" $mypath
+	wix save
+	
+
+elif [ "$1" = "new-git" ]; then
+	if [ "$2" = "gs" ]; then
+		echo "${GREEN}Generating new GetSkooled dir ($gs_path/$3)..."
+		mkdir $gs_path/$3
+		cd $gs_path/$3
+		echo "# $3" >> README.md
+		git init
+		git add .
+		git commit -m "first commit"
+		git remote add origin git@github.com:getskooled/$3.git
+		xdg-open https://github.com/organizations/getskooled/repositories/new
+	else
+		echo "${GREEN}This is only supported for gs currently"
+	fi
+	
 elif [ "$1" = "git-init" ]; then
 	if [ $num_args -gt 1 ]; then
 		git init
@@ -97,7 +148,7 @@ elif [ "$1" = "git-init" ]; then
 		git remote add origin git@github.com:hwixley/$2.git
 		xdg-open https://github.com/new
 	else
-		echo "Provide a repo name:${RESET}"
+		echo "${GREEN}Provide a repo name:${RESET}"
 		read name
 		git init
 		git add .
@@ -105,13 +156,14 @@ elif [ "$1" = "git-init" ]; then
 		git remote add origin git@github.com:hwixley/$name.git
 		xdg-open https://github.com/new
 	fi
+	
 elif [ "$1" = "push" ]; then
 	if [ $num_args -gt 1 ]; then
 		git add .
 		git commit -m "wix-cli quick commit"
 		git push origin $2
 	else
-		echo "Provide a branch name:${RESET}"
+		echo "${GREEN}Provide a branch name:${RESET}"
 		read name
 		if [ "$name" = "" ]; then
 			git add .
@@ -124,13 +176,12 @@ elif [ "$1" = "push" ]; then
 		fi
 	fi
 elif [ "$1" = "repo" ]; then
-	echo "repo"
+	echo "${GREEN}Redirecting to repo..."
 	remote=$(git config --get remote.origin.url | sed 's/.*\/\([^ ]*\/[^.]*\).*/\1/')
 	repo_url=${remote#"git@github.com:"}
 	repo_url=${repo_url%".git"}
 	xdg-open "https://github.com/$repo_url"
-elif [ "$1" = "back" ]; then
-	cd -
+	
 else
-	echo "Invalid command! Try again"
+	echo "${RED}Invalid command! Try again"
 fi
