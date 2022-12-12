337952fae22cf20b9ffef9424c76b4167f6e8a28
Harry Wixley <hwixley1@gmail.com>
2022-12-12 22:42:27 +0000

    wix-cli quick commit

--- setup.sh
+++ setup.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
+# COLORS
+GREEN=$(tput setaf 2)
+ORANGE=$(tput setaf 3)
+RED=$(tput setaf 1)
+BLUE=$(tput setaf 4)
+CYAN=$(tput setaf 6)
+BLACK=$(tput setaf 0)
+RESET=$(tput setaf 7)
+
+# FUNCTIONS
+function info_text() {
+	echo "${GREEN}$1${RESET}"
+}
+
+function h1_text() {
+	echo "${BLUE}$1${RESET}"
+}
+
+function warn_text() {
+	echo "${ORANGE}$1${RESET}"
+}
+
+info_text "Setting up wix-cli..."
+
 chmod +x wix-cli-template.sh
+
+
+# SETUP ENVFILE
 
 envfile=~/.bashrc
 
@@ -8,31 +35,61 @@ if [ "$(uname)" == "Darwin" ]; then
 	envfile=~/.zshrc
 fi
 
-if [ -d "$envfile" ]; then
+while ! [ -f "$envfile" ]; do
 	echo ""
-	echo "Please enter the path of your shell file:"
+	info_text "Please enter the path of your shell file:"
 	read -r shellpath
 	envfile=$shellpath
-else
-	echo ""
-	echo "Is $envfile the correct directory to be appending to? [ y / n]"
-	read response
 
-	if [ "$response" != "y" ]; then
-		echo ""
-		echo "Please enter the path of your shell file:"
-		read -r shellpath
-		envfile=$shellpath
+	if ! [ -f "$envfile" ]; then
+		warn_text "Error: that file does not exist, please try again"
 	fi
+done
+
+
+# SETUP METADATA FILES
+md_dir=.wix-cli-data
+mkdir $md_dir
+touch $md_dir/git-user.txt
+touch $md_dir/git-orgs.txt
+touch $md_dir/dir-aliases.txt
+touch $md_dir/run-configs.txt
+mkdir $md_dir/run-configs
+
+# GET USER SPECIFIC DETAILS
+echo ""
+h1_text "Please enter your github username:"
+read gituser
+echo "name=$gituser" >> $md_dir/git-user.txt
+
+echo ""
+h1_text "Please enter the default github organization you want to setup with this cli: (enter it's github username) ***leave empty if none***"
+read gitorg
+if [ "$gitorg" != "" ]; then
+	echo "default=$gitorg" >> $md_dir/git-orgs.txt
 fi
 
-echo "" >> "$envfile"
-echo "# WIX CLI" >> "$envfile"
-echo "alias wix=\"source $(pwd)/wix-cli-template.sh\"" >> "$envfile"
-source "$envfile"
+echo ""
+echo "The default directory aliases setup are as follows:"
+echo "1) docs = ~/Documents"
+echo "2) down = ~/Downloads"
+h1_text "Would you like to include these? [ y / n ]"
+read keep_default_diraliases
+if [ "$keep_default_diraliases" = "y" ]; then
+	echo "docs=~/Documents" >> $md_dir/dir-aliases.txt
+	echo "down=~/Downloads" >> $md_dir/dir-aliases.txt
+fi
 
-GREEN=$(tput setaf 2)
+# FINAL SETUP
 echo ""
-echo "${GREEN}WIX CLI successfully added to $envfile !"
+info_text "Okay we should be good to go!"
+
+# echo "" >> "$envfile"
+# echo "# WIX CLI" >> "$envfile"
+# echo "alias wix=\"source $(pwd)/wix-cli-template.sh\"" >> "$envfile"
+# source "$envfile"
+
 echo ""
-echo "Use 'wix' to get going :)"
+info_text "WIX CLI successfully added to $envfile !"
+info_text "Use 'wix' to get going :)"
+echo ""
