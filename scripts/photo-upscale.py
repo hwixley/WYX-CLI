ff56ad8ef4402bd968974eb50bb8e9013f5ac0bc
Harry Wixley <hwixley1@gmail.com>
2023-03-05 21:09:44 +0000

    fixed upscale to work with coloured images

--- scripts/photo-upscale.py
+++ scripts/photo-upscale.py
@@ -1,18 +1,17 @@
 import numpy as np
 import cv2
 import sys
+import os
 
 img_file = sys.argv[1]
 alpha = int(sys.argv[2])
 
+fname = os.path.splitext(img_file)[0]
 arr = cv2.imread(img_file)
 
 size = arr.shape
 x = size[0]
 y = size[1]
-
-myfunc = lambda x: 255 if x > 0 else 0
-myfunc_vec = np.vectorize(myfunc)
 
 upscaled_y = np.empty((x, y*alpha, 3))
 
@@ -20,8 +19,7 @@ offset = 0
 for col in range(y):
     for a in range(alpha):
         if offset < y*alpha:
-            copy = np.vectorize(myfunc)(arr[:,col,:])
-            upscaled_y[:,offset,:] = copy
+            upscaled_y[:,offset,:] = arr[:,col,:]
 
             offset += 1
 
@@ -36,4 +34,4 @@ for row in range(x):
             offset += 1
 
 
-cv2.imwrite(f"qr-{x*alpha}x{y*alpha}.png", upscaled_x)
+cv2.imwrite(f"{fname}-{x*alpha}x{y*alpha}.png", upscaled_x)
