98ea3e8bdfb129b0b97b640749932c6152c7fcf4
Harry Wixley <hwixley1@gmail.com>
2023-06-25 14:52:41 +0100

    GPT-commit: Update openai_service.py logic. Update the logic in the openai_service.py script to improve the functionality of the GPT-commit. The changes are staged and ready to be committed based on the output of `git status` command.

--- scripts/services/openai_service.py
+++ scripts/services/openai_service.py
@@ -40,13 +40,13 @@ class OpenAIService:
         return f"`git diff` output: {os.popen(f'{cd_cmd} && git diff').read()}. `git status` output: {os.popen(f'{cd_cmd} && git status').read()}."
     
     def get_commit_title(self):
-        title_prompt = f"Write a 1 line commit message (less than or equal to 60 characters) using the following bash git outputs. {self.get_git_diff()} You do not need to mention anything about the branch these changes were made on, and you should mention the reasoning for the modifications not just what files changed."
+        title_prompt = f"Write a 1 line commit message (less than or equal to 50 characters) technically describing the following bash git outputs. {self.get_git_diff()} You do not need to mention anything about the branch these changes were made on, and you should mention the reasoning for the modifications not just what files changed."
         title_response = self.get_response(title_prompt)
         return f"GPT-commit: {title_response}"
     
     def get_commit_description(self):
         title = self.get_commit_title()
-        description_prompt = f"Write a 2 line commit message using the following bash git outputs: {self.get_git_diff()}, and elaborates on the commit title: \"{title}\". You should mention the reasoning for the modifications not just what files changed."
+        description_prompt = f"Write a 2 line commit message technically describing the following bash git outputs: {self.get_git_diff()}, and elaborates on the commit title: \"{title}\". You should not repeat the commit title in this message, and you should mention the technical reasoning for the modifications not just what files changed."
         description_response = self.get_response(description_prompt)
         return (title, description_response)
     
