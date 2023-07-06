eef20970b82bf92dd29b80587bfa3def9a1b7d77
Harry Wixley <hwixley1@gmail.com>
2023-07-06 09:33:22 +0100

    mac specific wifi
    GPT-commit: Modified wix-cli.sh to fix a bug in the script related to the handling of file paths and improve overall functionality.

--- wix-cli.sh
+++ wix-cli.sh
@@ -810,7 +810,11 @@ elif [ "$1" = "ip" ]; then
 	echo ""
 
 elif [ "$1" = "wifi" ]; then
-	python3 "${scriptdir}/wifi_sniffer.py"
+	if mac; then
+		/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport scan
+	else
+		python3 "${scriptdir}/wifi_sniffer.py"
+	fi
 
 elif [ "$1" = "wpass" ]; then
 	info_text "Listing saved Wifi passwords:"
