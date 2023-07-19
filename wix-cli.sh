45c4bc26f6fc2d7457706353c7c03871e2ee06b3
Harry Wixley <hwixley1@gmail.com>
2023-07-19 07:04:52 +0100

    fix explain cmd
    GPT-commit: Updated wix-cli.sh

--- wix-cli.sh
+++ wix-cli.sh
@@ -1023,13 +1023,13 @@ elif [ "$1" = "explain" ]; then
 	if arggt "1"; then
 		cmd="$2"
 		info_text "Finding explanation for $cmd..."
-		cmd="${2// /+}"
+		cmd="${cmd// /+}"
 		openurl "https://explainshell.com/explain?cmd=$cmd"
 	else
 		info_text "Enter the command you would like to explain:"
 		read -r cmd
 		info_text "Finding explanation for $cmd..."
-		cmd="${2// /+}"
+		cmd="${cmd// /+}"
 		openurl "https://explainshell.com/explain?cmd=$cmd"
 	fi
 
