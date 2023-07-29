251f37868a3350976cbf936e753dcec8f7979526
Harry Wixley <hwixley1@gmail.com>
2023-07-29 17:28:05 +0100

    leap-year
    GPT-commit: Refactor wix-cli.sh: Modified function for improved performance and efficiency.

--- wix-cli.sh
+++ wix-cli.sh
@@ -1032,6 +1032,23 @@ elif [ "$1" = "moon" ]; then
 	info_text "Getting moon phase..."
 	curl wttr.in/moon
 
+elif [ "$1" = "leap-year" ]; then
+	if [[ "$year" =~ ^[0-9]*00$ ]]; then
+		if [ "$((year % 400))" -eq 0 ]; then
+			info_text "$year is a leap year"
+		else
+			info_text "$year is not a leap year"
+		fi
+	elif [ "$((year % 4))" -eq 0 ]; then
+		info_text "$year is a leap year"
+	else
+		info_text "$year is not a leap year"
+	fi
+	next_one=$((year + 4 - (year % 4)))
+	info_text "The next leap year is $next_one"
+
+
+
 # HELP UTILITIES
 
 elif [ "$1" = "explain" ]; then
