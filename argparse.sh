4a98d4fa1e66722d4ed85d51b455a6bbcef418bb
Harry Wixley <hwixley1@gmail.com>
2023-08-05 12:05:14 +0100

    optimizeautoupdate
    GPT-commit: Update functions in argparse.sh for improved argument parsing logic.

--- argparse.sh
+++ argparse.sh
@@ -349,42 +349,6 @@ giturl() {
 }
 
 
-# AUTO UPDATE CLI
-
-wix_update() {
-	info_text "Checking for updates..."
-	cd "$mydir" || return 1
-	git fetch
-	UPSTREAM=${1:-'@{u}'}
-	LOCAL=$(git rev-parse @)
-	REMOTE=$(git rev-parse "$UPSTREAM")
-	BASE=$(git merge-base @ "$UPSTREAM")
-
-	if [ "$LOCAL" = "$REMOTE" ]; then
-		info_text "Up-to-date"
-	elif [ "$LOCAL" = "$BASE" ]; then
-		info_text "Updating..."
-		pull "$branch"
-		# wix "${@:1}"
-		# exit
-	elif [ "$REMOTE" = "$BASE" ]; then
-		echo "Need to push"
-	else
-		echo "Diverged"
-	fi
-	echo ""
-	{
-		cd - || return 1
-	} &> /dev/null
-}
-
-# {
-# 	wix_update ""
-# } &> /dev/null
-
-# wix_update ""
-warn_text "Testing modular argparse autoupdate..."
-
 # DEFAULT
 
 
