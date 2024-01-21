0339965daf6abd48fc667b2805ac828b05d38d55
Harry Wixley <hwixley1@gmail.com>
2024-01-21 22:18:27 +0000

    GPT-commit: Modified `src/commands/webtext.sh` to optimize web text extraction functionality.
    GPT-commit: Optimized web text extraction functionality in `src/commands/webtext.sh`.

--- src/commands/webtext.sh
+++ src/commands/webtext.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if wixd.arggt "1"; then
-    wgit.wix_ginit "$1"
+    sys.webtext "$1"
 else
     sys.info "Enter the webpage you would like to parse:"
     read -r webpage
-    wgit.wix_ginit "$webpage"
+    sys.webtext "$webpage"
 fi
