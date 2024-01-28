4c956bce1041658002443fe8d14fb27c97c51bc3
Harry Wixley <hwixley1@gmail.com>
2024-01-28 14:42:30 +0000

    fixed zsh header loading
    GPT-commit: Modified classes cmd, lib, sys, wgit, and wixd in src for optimization.

--- src/classes/cmd/cmd.h
+++ src/classes/cmd/cmd.h
@@ -1,6 +1,6 @@
 cmd(){
     if [[ "$OSTYPE" == "darwin"* ]] || [[ "$(ps -o args= -p $$)" = *"zsh"* ]]; then
-        . <(sed "s/cmd/$1/g" "${0:A:h}/src/classes/cmd/cmd.class")
+        . <(sed "s/cmd/$1/g" "${WIX_DIR}/src/classes/cmd/cmd.class")
     else
         . <(sed "s/cmd/$1/g" $(dirname ${BASH_SOURCE[0]})/cmd.class)
     fi
--- src/classes/wixd/wixd.h
+++ src/classes/wixd/wixd.h
@@ -1,6 +1,6 @@
 wixd(){
     if [[ "$OSTYPE" == "darwin"* ]] || [[ "$(ps -o args= -p $$)" = *"zsh"* ]]; then
-        . <(sed "s/wixd/$1/g" "${0:A:h}/src/classes/wixd/wixd.class")
+        . <(sed "s/wixd/$1/g" "${WIX_DIR}/src/classes/wixd/wixd.class")
     else
         . <(sed "s/wixd/$1/g" $(dirname ${BASH_SOURCE[0]})/wixd.class)
     fi
