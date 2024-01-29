fcbde1aa63293fb08d1b55133fc5546c81c8d933
Harry Wixley <hwixley1@gmail.com>
2024-01-29 17:56:58 +0000

    GPT-commit: Modified `install-deps.sh` script to update dependencies.
    GPT-commit: Modified `install-deps.sh` script to update dependencies. This change improves the functionality of the script by updating the dependencies to the latest versions.

--- src/commands/install-deps.sh
+++ src/commands/install-deps.sh
@@ -2,14 +2,17 @@
 
 if ! sys.shell.zsh; then
     sys.info "Installing dependencies..."
-    sudo apt-get install xclip
+    sudo apt-get update
+    sudo apt-get -y install xclip jq csvkit
     curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
-    sudo apt-get install speedtest
+    sudo apt-get -y install speedtest
+
 fi
 
 if sys.os.mac; then
     sys.info "Installing dependencies..."
-    brew install xclip jq
+    brew update
+    brew install xclip jq csvkit
     brew tap teamookla/speedtest
     brew install speedtest --force
 fi
