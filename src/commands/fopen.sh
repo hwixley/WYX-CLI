029cc05d916c33a3b2a3768e8643d66c64cc72e5
Harry Wixley <hwixley1@gmail.com>
2024-02-24 20:06:20 +0000

    GPT-commit: Refactored README_TEMPLATE & modified fopen script logic
    Refactored logic in `README_TEMPLATE.md` for better readability and updated functionality for `fopen.sh` script to improve file opening efficiency.

--- src/commands/fopen.sh
+++ src/commands/fopen.sh
@@ -6,8 +6,11 @@ if wyxd.arggt "1"; then
         mydir="${mydirs[$dir]/\~/${HOME}}"
         sys.log.info "Opening $WYX_DIR..."
         sys.util.openfile "$WYX_DIR"
+    elif [ -f "$dir" ]; then
+        sys.log.info "Opening file..."
+        sys.util.openfile "$dir"
     else
-        sys.log.error "Directory alias does not exist"
+        sys.log.error "Invalid directory or file"
     fi
 else
     sys.log.info "Opening current directory..."
